b2d0ed5ba22c2bb58d5da2e15b2097a9ba10d038
Marta Tomczak <49766582+mtomchuck@users.noreply.github.com>
2020-04-12 13:59:45 +0200

    Welcome component content update

--- js/components/Welcome.js
+++ js/components/Welcome.js
@@ -4,18 +4,19 @@ const Welcome = () => {
     return (
         <div className="welcome">
             <a className="welcome-close-btn">close</a>
-            <h2>Hello Detective,</h2>
-            <h3>Welcome to Monster Village!</h3>
-            <p>These friendly little Monsters are harassed by unwelcomed guests.
+            <h1>Hello Detective!<br/>
+            Welcome to Monster Village</h1>
+            <p>These friendly little Monsters are harassed by unwelcomed guests.<br/>
             Only you can help them to get rid of sinister creatures with your JavaScript superpowers.</p>
             <div className="monsters">
                 <div>1</div>
                 <div>2</div>
                 <div>3</div>
             </div>
-            <p><b>RULES: </b>The game will teach you how to find and catch HTML elements using JavaScript functions. 
-            Read the instructions carefully and type your code to the console.</p>
-            <p>Make the Monster Village a better place! Enjoy.</p>
+            <p><b>LEARNING OBJECTIVES: </b>The game will teach you how to find HTML elements using predefined JavaScript functions. 
+            Read the instructions carefully and type your code to the console. Enjoy it.</p>
+            <p><b>PREREQUISITES: </b>For all JavaScript learners. Familiarity with the basics of HTML and JavaScript fundamentals is a plus but not required.</p>
+            <h3>Make the Monster Village a better place.</h3>
         </div>
     )
 }
