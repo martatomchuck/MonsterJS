2c471bc0f438090595faa8ebbcc8beb1ee41eca5
Marta Tomczak <49766582+mtomchuck@users.noreply.github.com>
2020-06-21 19:12:33 +0200

    updating footer

--- js/components/Footer.js
+++ js/components/Footer.js
@@ -3,7 +3,12 @@ import React, { useState, useContext } from "react";
 const Footer = () => {
     return (
         <footer className="footer">
-            <div className="footer-copyright">Created with <div className="footer-heart"></div> by <a href={"https://github.com/mtomchuck"} target={"_blank"}>mtomchuck</a> &bull; <a href={"https://github.com/mtomchuck/monster-village"} target={"_blank"}>GitHub</a></div>
+            <div className="footer-copyright">
+            Created with <div className="footer-heart"></div> by <a href={"https://github.com/mtomchuck"} target={"_blank"}>martatomchuck</a>
+            </div>
+            <div className="footer-github">
+                If you spot any bug or have feedback, please open an isssue on<a href={"https://github.com/mtomchuck/monster-village"} target={"_blank"}>GitHub</a>
+            </div>
             <div>Images made by Freepik & Smashicons & Dimitry Miroliubov from flaticon.com</div>
         </footer>
     )
