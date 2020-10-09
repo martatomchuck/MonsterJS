3ec18a8a1df834ddc8d440e50d756191598eabfa
Marta Tomczak <49766582+mtomchuck@users.noreply.github.com>
2020-10-09 23:22:48 +0200

    bugfix updated links

--- js/components/Footer.js
+++ js/components/Footer.js
@@ -8,10 +8,10 @@ const Footer = () => {
     return (
         <footer className="footer" onClick={closeCounterList}>
             <div className="footer-copyright">
-            Created with <div className="footer-heart"></div> by <a href={"https://github.com/mtomchuck"} target={"_blank"}>martatomchuck</a>
+            Created with <div className="footer-heart"></div> by <a href={"https://github.com/martatomchuck"} target={"_blank"}>martatomchuck</a>
             </div>
             <div className="footer-github">
-                If you spot any bug or have feedback, please open an isssue on<a href={"https://github.com/mtomchuck/monster-village"} target={"_blank"}>GitHub</a>
+                If you spot any bug or have feedback, please open an isssue on<a href={"https://github.com/martatomchuck/MonsterJS"} target={"_blank"}>GitHub</a>
             </div>
             <div>Images made by Freepik & Smashicons & Dimitry Miroliubov from flaticon.com</div>
         </footer>
